df20c3b1ff24d83cde13108b1e27dbee02ec9831
theendercore <theendercore@gmail.com>
2024-07-24 17:20:36 +0300

    - particles colors have been done

--- src/main/kotlin/org/teamvoided/template/EyeFailLogic.kt
+++ src/main/kotlin/org/teamvoided/template/EyeFailLogic.kt
@@ -1,25 +1,30 @@
 package org.teamvoided.template
 
 import net.minecraft.item.ItemUsageContext
-import net.minecraft.particle.ParticleTypes
+import net.minecraft.particle.DustParticleEffect
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
+import net.minecraft.util.math.Vec3d
 
 object EyeFailLogic {
     @JvmStatic
     fun disablePortal(c: ItemUsageContext) {
+        if (c.world.isClient) return
         val world = c.world as ServerWorld
         val pos = c.blockPos
 
         world.spawnParticles(
-            ParticleTypes.TRIAL_SPAWNER_DETECTION_OMINOUS,
+            DustParticleEffect(Vec3d.unpackRgb(0x69a395).toVector3f(), .7f),
             pos.x + 0.5,
-            pos.y + 1.0,
+            pos.y + (14.0 / 16),
             pos.z + 0.5,
-            10,
-            0.0,
+            5,
             0.1,
-            0.0,
-            0.1
+            0.1,
+            0.1,
+            0.01
         )
+        world.playSound(null, pos, SoundEvents.ENTITY_ENDER_EYE_DEATH, SoundCategory.BLOCKS, 0.8f, 0.1f)
     }
 }
